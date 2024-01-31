5398136cd4b67d0acb5b19bcb3355668ace0dac8
banset <mohitbasnet80@gmail.com>
2024-01-31 22:50:24 +0545

    Change in projects template

--- projects/urls.py
+++ projects/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from . import views
+from .views import ProjectView
 
 urlpatterns = [
     
     path('',views.projects, name = "projects"),
-    path('project/<str:pk>/', views.project, name = 'project'),
+    path('project/<str:pk>/',  ProjectView.as_view(), name = "project"),
     path('create-project/', views.createProject, name = "create-project"),
     path('update-project/<str:pk>/', views.updateProject , name = "update-project"),
     path('delete-project/<str:pk>/', views.deleteProject , name = "delete-project"),
