333c3e127d0c5b71e85c87e4659c77564e7b750c
banset <mohitbasnet80@gmail.com>
2024-02-01 22:00:33 +0545

    Projects

--- projects/views.py
+++ projects/views.py
@@ -9,7 +9,7 @@ from .utils import searchProjects,paginateProjects
 
 def projects(request):
     projects, search_query = searchProjects(request)
-    custom_range, projects = paginateProjects(request, projects, 6	    )
+    custom_range, projects = paginateProjects(request, projects, 6)
 
     context = {'projects': projects,
                'search_query': search_query, 'custom_range': custom_range}
