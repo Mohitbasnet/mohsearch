1b23ec9ae80361d2b451b56bb0d1c450f8ba2043
banset <mohitbasnet80@gmail.com>
2024-02-03 15:44:51 +0545

    Added class based view

--- projects/urls.py
+++ projects/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 from . import views
-from .views import ProjectView
+from .views import ProjectView,CreateProjectView
 
 urlpatterns = [
     
     path('',views.projects, name = "projects"),
     path('project/<str:pk>/',  ProjectView.as_view(), name = "project"),
-    path('create-project/', views.createProject, name = "create-project"),
+    path('create-project/',CreateProjectView.as_view(), name = "create-project"),
     path('update-project/<str:pk>/', views.updateProject , name = "update-project"),
     path('delete-project/<str:pk>/', views.deleteProject , name = "delete-project"),
 
--- projects/views.py
+++ projects/views.py
@@ -6,6 +6,7 @@ from .forms import ProjectForm, ReviewForm
 from django.contrib.auth.decorators import login_required
 from .utils import searchProjects,paginateProjects
 from django.views import View
+from django.utils.decorators import method_decorator
 # Create your views here.
 
 def projects(request):
@@ -62,23 +63,54 @@ class ProjectView(View):
 
 
  
-@login_required(login_url = "login")
-def createProject(request):
-    profile = request.user.profile
-    form = ProjectForm()
-    if request.method == 'POST':
-        newtags  = request.POST.get('newtags').replace(',', " ").split()
-        form = ProjectForm(request.POST , request.FILES)
+# @login_required(login_url = "login")
+# def createProject(request):
+#     profile = request.user.profile
+#     form = ProjectForm()
+#     if request.method == 'POST':
+#         newtags  = request.POST.get('newtags').replace(',', " ").split()
+#         form = ProjectForm(request.POST , request.FILES)
+#         if form.is_valid():
+#             project =  form.save(commit= False)
+#             project.owner =  profile
+#             project.save()
+#             for tag in newtags:
+#                 tag, created = Tag.objects.get_or_create(name = tag)
+#                 project.tags.add(tag)
+#             return redirect('account')
+#     context = {"form": form}
+#     return render(request,"projects/project_form.html", context)
+
+
+
+
+@method_decorator(login_required, name='dispatch')
+class CreateProjectView(View):
+    template_name = "projects/project_form.html"
+    def get(self, request):
+        form = ProjectForm()   # display form
+        context = {'form': form}
+        return render(request,self.template_name, context)
+    
+    def post(self, request):
+        form = ProjectForm(request.POST, request.FILES)  # handle the project form and project creation
         if form.is_valid():
-            project =  form.save(commit= False)
-            project.owner =  profile
+            project = form.save(commit =False)
+            project.owner = self.request.user.profile
             project.save()
+
+            newtags = self.request.POST.get('newtags').replace(',', " ").split()
             for tag in newtags:
-                tag, created = Tag.objects.get_or_create(name = tag)
+                tag, created = Tag.objects.get_or_create(name=tag)
                 project.tags.add(tag)
+
             return redirect('account')
-    context = {"form": form}
-    return render(request,"projects/project_form.html", context)
+        else:
+            context = {"form": form}
+            return render(request, self.template_name, context)
+        
+
+
 
 @login_required(login_url = "login")
 def updateProject(request,pk):
