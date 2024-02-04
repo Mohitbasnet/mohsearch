54c89e8781c12dfc0abc82f5b3f41371c766c1e3
banset <mohitbasnet80@gmail.com>
2024-02-04 23:24:49 +0545

    Changed

--- projects/urls.py
+++ projects/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
 from . import views
-from .views import ProjectView,CreateProjectView
+from .views import ProjectView,CreateProjectView,ProjectUpdateView
 
 urlpatterns = [
     
     path('',views.projects, name = "projects"),
     path('project/<str:pk>/',  ProjectView.as_view(), name = "project"),
-    path('create-project/',CreateProjectView.as_view(), name = "create-project"),
-    path('update-project/<str:pk>/', views.updateProject , name = "update-project"),
+    path('create-project/', CreateProjectView.as_view(), name = "create-project"),
+    path('update-project/<str:pk>/',ProjectUpdateView.as_view() , name = "update-project"),
     path('delete-project/<str:pk>/', views.deleteProject , name = "delete-project"),
 
 ]
