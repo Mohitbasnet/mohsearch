cfc51cd3cc9d251b01c58ebcb2e5abe7be689848
banset <mohitbasnet80@gmail.com>
2024-02-06 23:30:04 +0545

    Added Createskill class based view

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from .views import CreateSkillView
 
 urlpatterns = [
 
@@ -8,7 +9,7 @@ urlpatterns = [
     path('register/',views.registerUser, name = "register"),
     path('account/', views.userAccount, name = 'account'),
     path('edit-account/', views.editAccount, name = "edit-account"),
-    path('create-skill/',views.createSkill, name = "create-skill"),
+    path('create-skill/',CreateSkillView.as_view(), name = "create-skill"),
     path('update-skill/<str:pk>/',views.updateSkill, name = "update-skill"),
     path('delete-skill/<str:pk>/',views.deleteSkill, name = "delete-skill"),
 
